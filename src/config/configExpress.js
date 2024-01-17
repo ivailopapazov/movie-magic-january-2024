137635c34a5ce8834a219c6384578490f1750104
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2024-01-17 20:34:37 +0200

    Add body parser

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 
 function configExpress(app) {
-    app.use(express.static(path.resolve('src/public')))
+    app.use(express.static(path.resolve('src/public')));
+    app.use(express.urlencoded({ extended: false }));
 
     return app;
 }
