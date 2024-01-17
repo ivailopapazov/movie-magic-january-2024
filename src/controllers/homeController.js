b301d4306fb2dbb05148beca6213938c3187c00b
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2024-01-17 21:52:00 +0200

    Show dynamic movies on search page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -12,6 +12,12 @@ router.get('/about', (req, res) => {
     res.render('about');
 });
 
+router.get('/search', (req, res) => {
+    const movies = movieService.getAll();
+
+    res.render('search', { movies });
+});
+
 router.get('/404', (req, res) => {
     res.render('404');
 });
