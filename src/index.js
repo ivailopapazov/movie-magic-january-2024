7a629e7349d63f273cf296eda162065fe4fe4355
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2024-01-17 18:32:20 +0200

    Setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('init')
+const express = require('express');
+
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Home page');
+});
+
+app.listen(port, () => console.log(`Server is listening on port ${port}...`));
