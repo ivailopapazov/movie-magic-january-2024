04c06c7a819ce240c64e840531b25c3b5211bd6a
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2024-01-17 19:44:43 +0200

    Add 404 page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -8,4 +8,8 @@ router.get('/about', (req, res) => {
     res.render('about');
 });
 
+router.get('/404', (req, res) => {
+    res.render('404'); 
+});
+
 module.exports = router;
--- src/routes.js
+++ src/routes.js
@@ -6,4 +6,8 @@ const movieController = require('./controllers/movieController');
 router.use(homeController);
 router.use(movieController);
 
+router.get('*', (req, res) => {
+    res.redirect('/404');
+});
+
 module.exports = router;
