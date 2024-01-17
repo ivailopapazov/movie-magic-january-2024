96ff69cda40d27ac4ab6eaed9db17a8a260624bf
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2024-01-17 20:40:18 +0200

    Add hardcoded movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,4 +1,12 @@
-const movies = [];
+const movies = [{
+    title: 'Jungle Cuise',
+    genre: 'Adventure',
+    director: 'Spilberg',
+    date: '2019',
+    imageUrl: '/img/jungle-cruise.jpeg',
+    rating: '5',
+    description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.'
+}];
 
 exports.create = (movieData) => {
     movies.push(movieData);
