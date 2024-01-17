482271cdf0bd6e308d9c9d730bea34e608024d66
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2024-01-17 20:35:04 +0200

    Create movie action and service

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
 
+const movieService = require('../services/movieService');
+
 router.get('/create', (req, res) => {
     res.render('create');
+});
+
+router.post('/create', (req, res) => {
+    const newMovie = req.body;
+
+    movieService.create(newMovie);
+    
+    res.send('Movie should be created');
 });
 
 module.exports = router;
---
+++ src/services/movieService.js
@@ -0,0 +1,5 @@
+const movies = [];
+
+exports.create = (movieData) => {
+    movies.push(movieData);
+};
