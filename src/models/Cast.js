4976523899d357e3d9292b441741c564a2c6e434
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2024-01-24 19:57:22 +0200

    Create cast model

---
+++ src/models/Cast.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const castSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: true,
+    },
+    age: {
+        type: Number, 
+        required: true,
+        max: 120,
+        min: 14,
+    },
+    born: {
+        type: String,
+        required: true,
+    },
+    nameInMovies: {
+        type: String,
+        required: true,
+    }, 
+    castImage: {
+        type: String,
+        required: true,
+        validate: {
+            validator(value) {
+                return /^https?:\/\//.test(value);
+            },
+            message: (props) => `${props.value} is invalid url for the castImage!`
+        }
+    },
+});
+
+const Cast = mongoose.model('Cast', castSchema);
+
+module.exports = Cast;
