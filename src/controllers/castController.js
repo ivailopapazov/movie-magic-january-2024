b33453a4a48a06dd685a3f50a06009512ca3480f
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2024-01-24 19:46:37 +0200

    Add post action for cast c reate

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -4,4 +4,12 @@ router.get('/create', (req, res) => {
     res.render('cast/create');
 });
 
+router.post('/create', (req, res) => {
+    const body = req.body;
+
+    console.log(body);
+
+    res.redirect('/');
+});
+
 module.exports = router;
