63404680b6afa013069db80a81137577215cfade
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2024-01-31 20:38:15 +0200

    Add cookie parser to express

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 
 function configExpress(app) {
     app.use(express.static(path.resolve('src/public')));
     app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
 
     return app;
 }
