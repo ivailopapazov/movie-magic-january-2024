29206c28da8f59d5336f47cd3e09ba1c31509769
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2024-01-31 19:27:05 +0200

    Add login page

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -14,4 +14,8 @@ router.post('/register', async (req, res) => {
     res.redirect('/auth/login');
 });
 
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
 module.exports = router;
